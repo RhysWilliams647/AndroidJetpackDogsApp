c0f2fb952714ed60994cff230ab54c5d665af363
RhysWilliams647 <rhyswilliams647@googlemail.com>
2020-10-16 14:17:03 +0100

    first commit

---
+++ app/src/main/java/com/rhys/dogsapp/view/MainActivity.java
@@ -0,0 +1,16 @@
+package com.rhys.dogsapp.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.rhys.dogsapp.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
